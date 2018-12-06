ee5bd0910402f24d27cdf2a8f6e180048c5977a6
Sumit Maurya <sumit.maurya@nagarro.com>
2018-12-06 16:12:46 +0530

    this is Intial changes

---
+++ src/app/post-page/post-page.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: "sharexp-post-page",
+    templateUrl: "./post-page.component.html",
+    styleUrls: ["./post-page.component.css"]
+})
+
+export class PostPageComponent{
+
+}
